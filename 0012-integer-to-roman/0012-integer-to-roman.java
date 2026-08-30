cd08a29ac7667c83b3c42e0d7e75f8caf452dec4
Jatindra2 <saiprasanna2420@gmail.com>
2026-08-30 11:05:03 +0530

    Time: 2 ms (100%), Space: 46.2 MB (90.73%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -6,61 +6,72 @@ class Solution {
         return s;
     }
     public String intToRoman(int num) {
-        String s = "";
-        int n = 0;
+        int i = 0;
+        char[] ans = new char[16];
         if (num>=1000){
-            n = num/1000;
-            s = addString(s, "M", n);
-            num = num%1000;
+            while(num>=1000){
+                ans[i++] = 'M';
+                num -= 1000;
+            }
         }
         if (num>=900){
-            s += "CM";
+            ans[i++] = 'C';
+            ans[i++] = 'M';
             num -= 900;
         }
         if (num>=500){
-            s += "D";
+            ans[i++] = 'D';
             num -= 500;
         }
         if (num>=400){
-            s += "CD";
+            ans[i++] = 'C';
+            ans[i++] = 'D';
             num -= 400;
         }if (num>=100){
-            n = num/100;
-            s = addString(s, "C", n);
-            num = num%100;
+            while(num>=100){
+                ans[i++] = 'C';
+                num -= 100;
+            }
         }
         if (num>=90){
-            s += "XC";
+            ans[i++] = 'X';
+            ans[i++] = 'C';
             num -= 90;
         }
         if (num>=50){
-            s = addString(s, "L", 1);
+            ans[i++] = 'L';
             num -= 50;
         }
         if (num>=40){
-            s += "XL";
+            ans[i++] = 'X';
+            ans[i++] = 'L';
             num -= 40;
         }
         if (num>=10){
-            n = num/10;
-            s = addString(s, "X", n);
-            num = num%10;
+            while(num>=10){
+                ans[i++] = 'X';
+                num -= 10;
+            }
         }
         if (num == 9){
-            s += "IX";
+            ans[i++] = 'I';
+            ans[i++] = 'X';
             num -= 9;
         }
         if (num>=5){
-            s += "V";
+            ans[i++] = 'V';
             num -= 5;
         }
         if (num == 4){
-            s += "IV";
+            ans[i++] = 'I';
+            ans[i++] = 'V';
             num -= 4;
         } else {
-            s = addString(s, "I", num);
-            num = 0;
+            while(num>0){
+                ans[i++] = 'I';
+                num -= 1;
+            }
         }
-        return s;
+        return new String(ans, 0, i);
     }
 }
